feb9e4654676c8b91e2cfa528363b61943b85bc4
taehee <wonderfulhuman@naver.com>
2021-10-16 03:06:06 +0900

    Feat: Controller should return token when sign-up is succeed

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,7 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { SignUpDto } from './dto/sign.up.dto';
+import * as faker from 'faker';
 
+const mockUsersService = {
+  signUp: jest.fn(),
+};
 describe('UsersController', () => {
   let controller: UsersController;
   let service: UsersService;
@@ -9,7 +14,12 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -22,5 +32,33 @@ describe('UsersController', () => {
 
   it('should have service', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Sign-Up', () => {
+    it('should return token when sign-up is succeed', async () => {
+      // given
+      const shaAlgorithmValue = 256 / 4;
+
+      const signUpDTO: SignUpDto = {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        nickname: faker.internet.userName(),
+      };
+
+      const successResponse = {
+        success: true,
+        token: `${faker.datatype.hexaDecimal(
+          shaAlgorithmValue,
+        )}.${faker.datatype.hexaDecimal(
+          shaAlgorithmValue,
+        )}.${faker.datatype.hexaDecimal(shaAlgorithmValue)}`,
+      };
+
+      service.signUp = jest.fn().mockResolvedValueOnce(successResponse);
+      // when
+      const result = await controller.signUp(signUpDTO);
+      // then
+      expect(result).toBe(successResponse);
+    });
   });
 });
