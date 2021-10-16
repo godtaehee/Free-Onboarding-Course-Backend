4ea50343c9ba5c59610f2f33752e6a24166cfc47
taehee <wonderfulhuman@naver.com>
2021-10-16 10:00:03 +0900

    Test: Service should have repository && MockingRepository TestingMoudle에 주입

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+
+const mockUsersRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
-
+  let repository: UsersRepository;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UsersRepository,
+          useValue: mockUsersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should have repository ', () => {
+    expect(repository).toBeDefined();
   });
 });
