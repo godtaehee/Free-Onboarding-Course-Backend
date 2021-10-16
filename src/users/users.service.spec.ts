9ecf83def0f40559ec4029c7b65f9f2d55cc9400
taehee <wonderfulhuman@naver.com>
2021-10-16 10:48:00 +0900

    Test: Service should return token when sign-up is succeed

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
+import { SignUpDto } from './dto/sign.up.dto';
+import * as faker from 'faker';
 
 const mockUsersRepository = {
   create: jest.fn(),
   save: jest.fn(),
+  signUp: jest.fn(),
 };
 
 describe('UsersService', () => {
@@ -31,5 +34,35 @@ describe('UsersService', () => {
 
   it('should have repository ', () => {
     expect(repository).toBeDefined();
+  });
+
+  describe('Sign-up', () => {
+    it('should be return token when sign-up is succeed', async () => {
+      // given
+      const shaAlgorithmValue = 256 / 4;
+
+      const signUpDTO: SignUpDto = {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        nickname: faker.internet.userName(),
+      };
+
+      const successResponse = {
+        success: true,
+        token: `${faker.datatype.hexaDecimal(
+          shaAlgorithmValue,
+        )}.${faker.datatype.hexaDecimal(
+          shaAlgorithmValue,
+        )}.${faker.datatype.hexaDecimal(shaAlgorithmValue)}`,
+      };
+
+      repository.signUp = jest.fn().mockResolvedValueOnce(successResponse);
+
+      // when
+      const result = await service.signUp(signUpDTO);
+
+      // then
+      expect(result).toBe(successResponse);
+    });
   });
 });
