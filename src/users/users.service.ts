ca563bfdba0a66e4a345af7559d175cd13d23bec
taehee <wonderfulhuman@naver.com>
2021-10-16 10:48:52 +0900

    Feat: UsersRepository를 UsersService에 주입 && signUp Method 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpDto } from './dto/sign.up.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  signUp(signUpDto: SignUpDto) {}
+  constructor(
+    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+  ) {}
+  signUp(signUpDto: SignUpDto) {
+    return this.usersRepository.signUp(signUpDto);
+  }
 }
