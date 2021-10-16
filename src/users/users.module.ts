e6fc36eb3ab964bac4b075315da044fded892a2c
taehee <wonderfulhuman@naver.com>
2021-10-16 18:04:45 +0900

    Feat: Helper를 UsersModule에 import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users.entity';
 import { UsersRepository } from './users.repository';
+import { UtilsHelper } from '../common/utils/utils.helper';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersRepository])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UtilsHelper],
 })
 export class UsersModule {}
