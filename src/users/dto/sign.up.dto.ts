1c1c0b3438b879e5e63ef405f39bd67fc530e736
taehee <wonderfulhuman@naver.com>
2021-10-16 11:48:51 +0900

    Feat: DTO Validation 추가

--- src/users/dto/sign.up.dto.ts
+++ src/users/dto/sign.up.dto.ts
@@ -1,7 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class SignUpDto {
+  @IsNotEmpty()
+  @IsEmail()
   public email: string;
 
+  @IsNotEmpty()
+  @IsString()
   public nickname: string;
 
+  @IsNotEmpty()
+  @IsString()
   public password: string;
 }
