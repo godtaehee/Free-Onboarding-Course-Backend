0a99d212bb7895368733b0964f244ef0a9e184f3
taehee <wonderfulhuman@naver.com>
2021-10-16 21:06:01 +0900

    Refactor: SignUpDTO import 경로 변경

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './users.entity';
-import { SignUpDto } from './dto/sign.up.dto';
+import { SignUpDto } from '../auth/dto/sign.up.dto';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
