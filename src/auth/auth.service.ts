399ba83d6ffd7a0d2ad2298d5dfe633a957b75eb
taehee <wonderfulhuman@naver.com>
2021-10-16 22:57:16 +0900

    Feat: Auth Service signIn method로 로그인 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from '../users/users.repository';
 import { SignUpDto } from './dto/sign.up.dto';
 import { SignInDto } from './dto/sign.in.dto';
 import * as bcrypt from 'bcrypt';
 import { UtilsHelper } from '../common/utils/utils.helper';
+import { User } from '../users/users.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersRepository: UsersRepository,
+    private jwtService: JwtService,
     private utilsHelper: UtilsHelper<SignUpDto>,
   ) {}
 
@@ -21,5 +24,14 @@ export class AuthService {
     return this.usersRepository.signUp(clonedSignUpDto);
   }
 
-  signIn(signInDto: SignInDto) {}
+  async signIn(signInDto: SignInDto) {
+    const { email, password } = signInDto;
+    const user: User = await this.usersRepository.signIn(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { email };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
+    } else throw new UnauthorizedException('logIn failed');
+  }
 }
