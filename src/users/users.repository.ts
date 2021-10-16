a17a3f60e648e0cb9dcf9303ad79934da9efe540
taehee <wonderfulhuman@naver.com>
2021-10-16 10:53:40 +0900

    Feat: UsersRepository signUp method 기본틀 작성

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,5 +1,15 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './users.entity';
+import { SignUpDto } from './dto/sign.up.dto';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async signUp(signUpDto: SignUpDto) {
+    const user = this.create(signUpDto);
+    try {
+      return await this.save(user);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
