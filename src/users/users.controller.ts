59b6e32d8457c4b5a0ad68d66ab9a1fb4df5fb05
taehee <wonderfulhuman@naver.com>
2021-10-16 20:59:37 +0900

    Refactor: users.controller.ts의 Route Handler 모두 삭제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,4 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { SignUpDto } from './dto/sign.up.dto';
-import { SignInDto } from './dto/sign.in.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-  constructor(private usersService: UsersService) {}
-
-  @Post('sign-up')
-  signUp(@Body(ValidationPipe) signUpDto: SignUpDto) {
-    return this.usersService.signUp(signUpDto);
-  }
-
-  @Post('sign-in')
-  signIn(signInDto: SignInDto) {
-    return this.usersService.signIn(signInDto);
-  }
-}
+export class UsersController {}
