68d3a6fdefd1017970776aa0e1b95e97736e1ce6
taehee <wonderfulhuman@naver.com>
2021-10-16 21:54:15 +0900

    Feat: JwtModule 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthService } from './auth.service';
 import { UtilsHelper } from '../common/utils/utils.helper';
 import { UsersRepository } from '../users/users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UsersRepository]),
+    JwtModule.register({
+      secret: `${process.env.JWT_SECRET_KEY}`,
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UtilsHelper],
 })
