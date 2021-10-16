55e9dfbc1fb71d3cb15cf799fa330dfeee5e620e
taehee <wonderfulhuman@naver.com>
2021-10-16 20:33:53 +0900

    Feat: Auth Domain에서 사용하기위한 UsersRepository export

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersRepository } from './users.repository';
 import { UtilsHelper } from '../common/utils/utils.helper';
+import { User } from './users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, UtilsHelper],
+  exports: [TypeOrmModule],
 })
 export class UsersModule {}
