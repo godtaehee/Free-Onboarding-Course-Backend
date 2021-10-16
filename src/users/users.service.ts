fefb5c32f31e8aba901db3d6a3a24b7d6a6c7e94
taehee <wonderfulhuman@naver.com>
2021-10-16 21:09:43 +0900

    Refactor: users.service.ts의 내용 모두 삭제

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { SignUpDto } from './dto/sign.up.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UsersRepository } from './users.repository';
-import * as bcrypt from 'bcrypt';
-import { UtilsHelper } from '../common/utils/utils.helper';
-import { SignInDto } from './dto/sign.in.dto';
 
 @Injectable()
-export class UsersService {
-  constructor(
-    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
-    private utilsHelper: UtilsHelper<SignUpDto>,
-  ) {}
-  async signUp(signUpDto: SignUpDto) {
-    const { password } = signUpDto;
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-    let clonedSignUpDto = this.utilsHelper.getDeepCloneObject(signUpDto);
-    clonedSignUpDto.password = hashedPassword;
-    return this.usersRepository.signUp(clonedSignUpDto);
-  }
-
-  signIn(signInDto: SignInDto) {}
-}
+export class UsersService {}
