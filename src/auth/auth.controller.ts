6c20ed60906a12578061f40c5cd9331d84dd6e6f
taehee <wonderfulhuman@naver.com>
2021-10-17 20:13:58 +0900

    Feat: sign-up && sign-in Log추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,30 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Inject, Post, ValidationPipe } from '@nestjs/common';
 import { SignUpDto } from './dto/sign.up.dto';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign.in.dto';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
 
   @Post('sign-up')
   signUp(@Body(ValidationPipe) signUpDto: SignUpDto) {
+    this.logger.debug(
+      `${new Date().toLocaleString()} '${signUpDto.email}' request sign-up`,
+    );
     return this.authService.signUp(signUpDto);
   }
 
   @Post('sign-in')
   signIn(@Body(ValidationPipe) signInDto: SignInDto) {
+    this.logger.debug(
+      `${new Date().toLocaleString()} '${signInDto.email}' request sign-in`,
+    );
     return this.authService.signIn(signInDto);
   }
 }
