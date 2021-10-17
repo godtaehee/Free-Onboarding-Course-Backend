24a9f07f87f3d45015b6a31d53a734b453db2189
taehee <wonderfulhuman@naver.com>
2021-10-17 21:57:23 +0900

    Feat: BoardsService에서 BoardsRepository 사용을 위한 import

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardsRepository } from './boards.repository';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forFeature([BoardsRepository]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardsRepository } from './boards.repository';
+import { BoardCreateDto } from './dto/board.create.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  constructor(
+    @InjectRepository(BoardsRepository)
+    private boardsRepository: BoardsRepository,
+  ) {}
+
+  createBoard(user, boardCreateDto: BoardCreateDto) {}
+}
