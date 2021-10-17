582a528a6018bac662b35242e02c1e7d55f49c78
taehee <wonderfulhuman@naver.com>
2021-10-17 21:50:13 +0900

    Test: BoardsController should return success property and boardId

--- src/boards/boards.controller.spec.ts
+++ src/boards/boards.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import * as faker from 'faker';
+import { BoardCreateDto } from './dto/board.create.dto';
 
 const mockWinston = {
   debug: jest.fn(),
@@ -40,5 +42,34 @@ describe('BoardsController', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Create-Board', () => {
+    it('should return success property and boardId', async () => {
+      // given
+      const requestUser = {
+        id: faker.datatype.number(),
+        email: faker.internet.email(),
+        nickname: faker.internet.userName(),
+        password: faker.internet.password(),
+      };
+      const boardCreateDto: BoardCreateDto = {
+        title: faker.name.title(),
+        content: faker.lorem.sentences(),
+      };
+
+      const successResponse = {
+        success: true,
+        boardId: faker.datatype.number(),
+      };
+
+      service.createBoard = jest.fn().mockReturnValueOnce(successResponse);
+
+      // when
+      const result = await controller.createBoard(requestUser, boardCreateDto);
+
+      // then
+      expect(result).toBe(successResponse);
+    });
   });
 });
