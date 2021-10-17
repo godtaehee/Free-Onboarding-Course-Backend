6dda1785ba7908c6ec0de47d786787dc71247e34
taehee <wonderfulhuman@naver.com>
2021-10-18 03:22:12 +0900

    Feat: 특정 User의 Board를 가져오기위한 BoardsService에 로직추가, 자기것이 아닌 게시글을 업데이트 시도시 UnauthorizedException 발생

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardsRepository } from './boards.repository';
 import { BoardCreateDto } from './dto/board.create.dto';
+import { BoardUpdateDto } from './dto/board.update.dto';
 
 @Injectable()
 export class BoardsService {
@@ -16,5 +17,21 @@ export class BoardsService {
 
   getSingleBoard(boardId: number) {
     return this.boardsRepository.getSingleBoard(boardId);
+  }
+
+  async updateBoard(
+    userId: number,
+    boardId: number,
+    updateRequestBody: BoardUpdateDto,
+  ) {
+    const selectedBoard = await this.boardsRepository.getBoardSpecificUser(
+      userId,
+      boardId,
+    );
+
+    if (!selectedBoard)
+      throw new UnauthorizedException('해당 게시글을 작성하신 분이 아닙니다.');
+
+    return this.boardsRepository.updateBoard(selectedBoard, updateRequestBody);
   }
 }
