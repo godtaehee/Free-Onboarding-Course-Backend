684b697c26b613a4f7d881b4d4874d0e632f842c
taehee <wonderfulhuman@naver.com>
2021-10-17 21:22:06 +0900

    Test: BoardsService의 BoardsRepository Mocking

--- src/boards/boards.service.spec.ts
+++ src/boards/boards.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardsService } from './boards.service';
-
+import { BoardsRepository } from './boards.repository';
+const mockBoardsRepository = {
+  save: jest.fn(),
+};
 describe('BoardsService', () => {
   let service: BoardsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardsService],
+      providers: [
+        BoardsService,
+        {
+          provide: BoardsRepository,
+          useValue: mockBoardsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<BoardsService>(BoardsService);
