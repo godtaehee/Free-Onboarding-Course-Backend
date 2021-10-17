5c8c759d81af684d556b3cdf739b71dc77365b71
taehee <wonderfulhuman@naver.com>
2021-10-17 22:09:40 +0900

    Feat: BoardsRepository의 기본 Board생성 로직 작성

--- src/boards/boards.repository.ts
+++ src/boards/boards.repository.ts
@@ -1,5 +1,25 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './boards.entity';
+import { BoardCreateDto } from './dto/board.create.dto';
 
 @EntityRepository(Board)
-export class BoardsRepository extends Repository<Board> {}
+export class BoardsRepository extends Repository<Board> {
+  async createBoard(user, boardCreateDto: BoardCreateDto) {
+    const { title, content } = boardCreateDto;
+
+    const createdBoard = this.create({
+      title,
+      content,
+      user,
+    });
+    try {
+      const savedBoard = await this.save(createdBoard);
+      return {
+        success: true,
+        boardId: savedBoard.id,
+      };
+    } catch (err) {
+      throw err;
+    }
+  }
+}
