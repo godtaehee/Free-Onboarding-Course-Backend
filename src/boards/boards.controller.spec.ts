202d280ba39b55e1ce39154dc2ec7463e9f43832
taehee <wonderfulhuman@naver.com>
2021-10-17 21:10:10 +0900

    Test: BoardsService should be defined in BoardsController

--- src/boards/boards.controller.spec.ts
+++ src/boards/boards.controller.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+
+const mockWinston = {
+  debug: jest.fn(),
+};
 
 describe('BoardsController', () => {
   let controller: BoardsController;
+  let service: BoardsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BoardsController],
+      providers: [
+        BoardsService,
+        {
+          provide: WINSTON_MODULE_PROVIDER,
+          useValue: mockWinston,
+        },
+      ],
     }).compile();
 
     controller = module.get<BoardsController>(BoardsController);
+    service = module.get<BoardsService>(BoardsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
