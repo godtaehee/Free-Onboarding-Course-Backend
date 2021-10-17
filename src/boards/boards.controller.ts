341edd36a8350b468aa0f3f4d3842f381aee1656
taehee <wonderfulhuman@naver.com>
2021-10-17 21:53:31 +0900

    Feat: POST /boards/create Route Handler 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  Logger,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../common/decorators/get-user.decorator';
+import { BoardsService } from './boards.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { BoardCreateDto } from './dto/board.create.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(
+    private boardsService: BoardsService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  @Post('/create')
+  createBoard(@GetUser() user, @Body() boardCreateDto: BoardCreateDto) {
+    this.logger.debug(
+      `${new Date().toLocaleString()} userId: '${
+        user.id
+      }' request board-create`,
+    );
+
+    return this.boardsService.createBoard(user, boardCreateDto);
+  }
+}
