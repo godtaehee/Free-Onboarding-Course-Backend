dc57d82d46b6beb3098acb99d4ef331b8800fb97
taehee <wonderfulhuman@naver.com>
2021-10-18 20:08:05 +0900

    Feat: 회원가입 성공 Response 작성

---
+++ src/common/response/user/register.success.response.ts
@@ -0,0 +1,16 @@
+import { Board } from '../../../boards/boards.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RegisterSuccessResponse {
+  @ApiProperty({
+    title: '요청 성공 여부',
+    example: 'true',
+  })
+  success: boolean;
+
+  @ApiProperty({
+    description: '회원가입이 성공한후 Auto-Generated된 유저의 고유 id값입니다.',
+    example: '1',
+  })
+  userId: number;
+}
