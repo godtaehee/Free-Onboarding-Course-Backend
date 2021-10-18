524f024755b92ec7463b9299d7570526f8435061
taehee <wonderfulhuman@naver.com>
2021-10-18 20:09:10 +0900

    Feat GET /?limit&offset Route Handler에 Swagger Description 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -20,7 +20,17 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { BoardCreateDto } from './dto/board.create.dto';
 import { BoardUpdateDto } from './dto/board.update.dto';
 import { User } from '../users/users.entity';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ReadAllBoardResponse } from '../common/response/board/read.all.board.response';
+import { PaginationDto } from './dto/pagination.dto';
 
+@ApiTags('게시글')
 @Controller('boards')
 export class BoardsController {
   constructor(
@@ -47,9 +57,26 @@ export class BoardsController {
     return this.boardsService.getSingleBoard(boardId);
   }
 
+  @ApiQuery({
+    name: 'limit',
+    required: true,
+    description: '최대 몇개의 Board를 가져올것인지에 대한 값입니다.',
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: true,
+    description:
+      '게시판의 시작페이지의 수를 나타냅니다. 0을 포함한 양수의 값입니다.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '성공적으로 Board를 가져왔을때의 응답입니다.',
+    type: ReadAllBoardResponse,
+  })
   @Get('/')
-  getAllBoard(@Query() query) {
-    this.logger.debug(query);
+  getAllBoard(
+    @Query(ValidationPipe) query: PaginationDto,
+  ): Promise<ReadAllBoardResponse> {
     return this.boardsService.getAllBoard(query.limit, query.offset);
   }
 
