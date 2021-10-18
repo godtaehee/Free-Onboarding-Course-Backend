a057e85cadb6d6235f08bb0dfe09f3c19fb13f53
taehee <wonderfulhuman@naver.com>
2021-10-19 04:18:33 +0900

    Feat: @ApiCommonResponseForm 데코레이터를 적용하여 다양한 데이터 응답바디를 받을수 있게 함

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Inject, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  Post,
+  UseInterceptors,
+  ValidationPipe,
+} from '@nestjs/common';
 import { SignUpDto } from './dto/sign.up.dto';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign.in.dto';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RegisterSuccessResponse } from '../common/response/user/register.success.response';
 import { SignInSuccessResponse } from '../common/response/user/sign.in.success.response';
+import { CommonResponseFormInterceptor } from '../common/interceptors/common.response.form.interceptor';
+import { ApiCommonResponseForm } from '../common/decorators/api.common.response.form';
 
+@UseInterceptors(CommonResponseFormInterceptor)
 @ApiTags('회원가입 & 로그인')
 @Controller('auth')
 export class AuthController {
@@ -26,11 +36,7 @@ export class AuthController {
       '회원가입을 하지않아도 게시글을 볼수는 있습니다. ' +
       '하지만 회원가입을 해야 게시글을 작성, 수정, 삭제를 할수 있습니다.',
   })
-  @ApiResponse({
-    status: 201,
-    description: '성공적으로 Board를 가져왔을때의 응답입니다.',
-    type: RegisterSuccessResponse,
-  })
+  @ApiCommonResponseForm(RegisterSuccessResponse)
   @Post('sign-up')
   signUp(@Body(ValidationPipe) signUpDto: SignUpDto) {
     this.logger.debug(
@@ -49,11 +55,7 @@ export class AuthController {
       '로그인 후 이 토큰을 이용해야만 게시글을 작성, 수정, 삭제를 할수 있습니다. ' +
       '토큰은 1시간뒤 만료됩니다.',
   })
-  @ApiResponse({
-    status: 201,
-    description: '성공적으로 AccessToken을 발급 받았을때의 응답입니다.',
-    type: SignInSuccessResponse,
-  })
+  @ApiCommonResponseForm(SignInSuccessResponse)
   @Post('sign-in')
   signIn(@Body(ValidationPipe) signInDto: SignInDto) {
     this.logger.debug(
