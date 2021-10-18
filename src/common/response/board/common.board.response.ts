cc70fccc19d37d77765cdcfa2e8f1ea6033b8280
taehee <wonderfulhuman@naver.com>
2021-10-18 22:14:32 +0900

    Feat: CreateBoardResponse를 CommonBoardResponse로 변경

--- src/common/response/board/common.board.response.ts
+++ src/common/response/board/common.board.response.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateBoardResponse {
+export class CommonBoardResponse {
   @ApiProperty({
     title: '요청 성공 여부',
     example: 'true',
@@ -8,7 +8,7 @@ export class CreateBoardResponse {
   success: boolean;
 
   @ApiProperty({
-    title: '생성된 Board의 Auto-Generated된 고유 Id',
+    title: '해당 요청처리가 완료된 Board의 Auto-Generated된 고유 Id',
     example: '1',
   })
   boardId: number;
