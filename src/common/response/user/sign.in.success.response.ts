e399f721e4613f4811ccae4494f061efbf9cc706
taehee <wonderfulhuman@naver.com>
2021-10-19 04:14:21 +0900

    Feat: SignInSuccessResponse Schema로 먼저 만들어놓는다. Sign-In에서 사용할것이기 때문이다.

--- src/common/response/user/sign.in.success.response.ts
+++ src/common/response/user/sign.in.success.response.ts
@@ -1,13 +1,8 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 import * as faker from 'faker';
 
+@ApiExtraModels()
 export class SignInSuccessResponse {
-  @ApiProperty({
-    title: '요청 성공 여부',
-    example: 'true',
-  })
-  success: boolean;
-
   @ApiProperty({
     description: '로그인 후 다양한 API에 접근할수 있는 AccessToken입니다.',
     example: `${faker.datatype.hexaDecimal(
