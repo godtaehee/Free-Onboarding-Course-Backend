e91d7e579d1568a80b885a4bf991075c54217c89
taehee <wonderfulhuman@naver.com>
2021-10-18 15:34:45 +0900

    Feat: Seeding을 위한 Board Factory 생성

---
+++ src/database/factories/board.factories.ts
@@ -0,0 +1,21 @@
+import { define, factory } from 'typeorm-seeding';
+import * as faker from 'faker';
+import { Board } from '../../boards/boards.entity';
+
+define(Board, () => {
+  const title = faker.lorem.sentence();
+  const content = faker.lorem.sentences();
+
+  const board = new Board();
+
+  board.title = title;
+  board.content = content;
+  board.user = {
+    id: faker.datatype.number({
+      min: 1,
+      max: 20,
+    }),
+  } as any;
+
+  return board;
+});
