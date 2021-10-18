5dd932788dfd6ac1bc229045a0be254fd5c9e7f0
taehee <wonderfulhuman@naver.com>
2021-10-18 21:03:04 +0900

    Feat: ResponseBody를 SignInSuccessResponse Type으로 반환

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { SignInDto } from './dto/sign.in.dto';
 import * as bcrypt from 'bcrypt';
 import { User } from '../users/users.entity';
 import { JwtService } from '@nestjs/jwt';
+import { SignInSuccessResponse } from '../common/response/user/sign.in.success.response';
 
 @Injectable()
 export class AuthService {
@@ -24,7 +25,12 @@ export class AuthService {
     if (user && (await bcrypt.compare(password, user.password))) {
       const payload = { email };
       const accessToken = this.jwtService.sign(payload);
-      return { accessToken };
+
+      const responseBody: SignInSuccessResponse = {
+        success: true,
+        accessToken,
+      };
+      return responseBody;
     } else throw new UnauthorizedException('logIn failed');
   }
 }
