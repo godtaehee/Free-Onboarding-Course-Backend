cf70875830c823981cc314ce997181773d20dd31
taehee <wonderfulhuman@naver.com>
2021-10-19 10:34:04 +0900

    Feat: @ApiCommonCreateResponseForm에 Omit<ApiResponseOptions, 'schema'> 타입의 옵션 추가

--- src/common/decorators/api.common.create.response.form.ts
+++ src/common/decorators/api.common.create.response.form.ts
@@ -1,11 +1,14 @@
 import { applyDecorators, Type } from '@nestjs/common';
 import { ApiCreatedResponse, getSchemaPath } from '@nestjs/swagger';
+import { ApiResponseOptions } from '@nestjs/swagger/dist/decorators/api-response.decorator';
 
 export const ApiCommonCreateResponseForm = <TModel extends Type<any>>(
   model: TModel,
+  options?: Omit<ApiResponseOptions, 'schema'>,
 ) => {
   return applyDecorators(
     ApiCreatedResponse({
+      ...options,
       schema: {
         allOf: [
           {
