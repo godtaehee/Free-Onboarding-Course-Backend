59908084edda65a0bcf7fb582449a6c34086dc4a
taehee <wonderfulhuman@naver.com>
2021-10-20 03:27:31 +0900

    Test: [E2E] /boards/:boardId (GET)

---
+++ test/app.query.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/boards/:boardId (GET)', async () => {
+    const result = await request(app.getHttpServer()).get('/boards/1');
+
+    expect(result.body.success).toBeTruthy();
+  });
+});
