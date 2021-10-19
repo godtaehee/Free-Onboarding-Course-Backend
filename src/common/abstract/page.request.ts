c694db8172108ce680fe3ddfe042e606e22a21aa
taehee <wonderfulhuman@naver.com>
2021-10-19 16:01:28 +0900

    Feat: PageRequest Abstract Class에 class-validator 데코레이터 적용 해당 Request를 받는 모든 Route Handler는 자동으로 Validation이 가능하다.

--- src/common/abstract/page.request.ts
+++ src/common/abstract/page.request.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsPositive } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export abstract class PageRequest {
+  @IsNotEmpty()
+  @IsPositive()
+  @Type(() => Number)
   offset: number | 1;
+
+  @IsNotEmpty()
+  @IsPositive()
+  @Type(() => Number)
   limit: number | 10;
 
   getOffset(): number {
