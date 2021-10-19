69862e0877ae24c9fee24b30a9db6d91d0a27145
taehee <wonderfulhuman@naver.com>
2021-10-19 16:02:59 +0900

    Feat: class-validator 데코레이터 PageRequest로 모두 이동

--- src/boards/dto/board.search.request.ts
+++ src/boards/dto/board.search.request.ts
@@ -1,15 +1,9 @@
-import { IsNotEmpty, IsPositive } from 'class-validator';
-import { Type } from 'class-transformer';
 import { PageRequest } from '../../common/abstract/page.request';
 
 export class BoardSearchRequest extends PageRequest {
-  @IsNotEmpty()
-  @IsPositive()
-  @Type(() => Number)
-  limit: number;
+  constructor() {
+    super();
+  }
 
-  @IsNotEmpty()
-  @IsPositive()
-  @Type(() => Number)
-  offset: number;
+  // 추후 검색 필터 데이터를 추가로 넣어도 됨 ex) title, content
 }
