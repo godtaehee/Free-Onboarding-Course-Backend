1ad6dfd610f23b28c1329633ddfc9b838d7fcdff
taehee <wonderfulhuman@naver.com>
2021-10-19 10:54:25 +0900

    Feat: CommonBoardResponse Schema로 만들기위한 Extra처리

--- src/common/response/board/common.board.response.ts
+++ src/common/response/board/common.board.response.ts
@@ -1,5 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 
+@ApiExtraModels()
 export class CommonBoardResponse {
   @ApiProperty({
     title: '해당 요청처리가 완료된 Board의 Auto-Generated된 고유 Id',
