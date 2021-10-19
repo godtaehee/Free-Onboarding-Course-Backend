cbba5a4b13d23ac328de7192b653010f843d366c
taehee <wonderfulhuman@naver.com>
2021-10-19 15:58:42 +0900

    Feat: CQRS 적용을 위해 Command요청이 아닌 Query요청만 따로 관리하는 QueryRepository 생성

---
+++ src/boards/boards.query.repository.ts
@@ -0,0 +1,31 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Board } from './boards.entity';
+import { BoardSearchRequest } from './dto/board.search.request';
+
+@EntityRepository(Board)
+export class BoardsQueryRepository extends Repository<Board> {
+  async getSingleBoard(boardId: number) {
+    return this.createQueryBuilder('boards')
+      .innerJoinAndSelect('boards.user', 'user')
+      .select(['boards', 'user.id', 'user.nickname'])
+      .where({ id: boardId })
+      .getOne();
+  }
+
+  async getBoardSpecificUser(userId: number, boardId: number) {
+    return this.createQueryBuilder('boards')
+      .innerJoinAndSelect('boards.user', 'user')
+      .select(['boards', 'user.id', 'user.nickname'])
+      .where('user.id = :userId AND boards.id = :boardId', { userId, boardId })
+      .getOne();
+  }
+
+  getAllBoard(query: BoardSearchRequest): Promise<[Board[], number]> {
+    return this.createQueryBuilder('boards')
+      .innerJoinAndSelect('boards.user', 'user')
+      .select(['boards', 'user.id', 'user.nickname'])
+      .limit(query.getLimit())
+      .offset(query.getOffset())
+      .getManyAndCount();
+  }
+}
