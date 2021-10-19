87c95a8603f1c7b874ad09e6d99e18165ea50c6a
taehee <wonderfulhuman@naver.com>
2021-10-19 21:59:50 +0900

    Feat: 유효하지 않은 숫자가 요청으로 들어온다면 BadRequestException

---
+++ src/common/response/error/not.valid.number.error.ts
@@ -0,0 +1,17 @@
+import { ApiExtraModels, ApiProperty, PickType } from '@nestjs/swagger';
+import { FourHundredError } from './four.hundred.error';
+
+@ApiExtraModels()
+export class NotValidNumberError extends PickType(FourHundredError, [
+  'statusCode',
+  'error',
+] as const) {
+  @ApiProperty({
+    type: 'string',
+    title: 'Error 메시지',
+    example: '잘못된 요청입니다.',
+    description:
+      'Request-Body의 각각 프로퍼티의 제약조건에 맞지않은 값이 포함되어 있습니다.',
+  })
+  message: any;
+}
