81ad78601475f12d50971899a93070db3aecc045
taehee <wonderfulhuman@naver.com>
2021-10-20 03:17:13 +0900

    Test: Login시 AccessToken을 따로 전역으로 저장 && 로그인 E2E Test 작성

--- test/app.command.e2e-spec.ts
+++ test/app.command.e2e-spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { Connection, QueryRunner } from 'typeorm';
 import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e) [COMMAND]', () => {
   let app: INestApplication;
   let queryRunner: QueryRunner;
+  let accessToken: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,6 +21,18 @@ describe('AppController (e2e) [COMMAND]', () => {
     const dbConnection = moduleFixture.get(Connection);
 
     queryRunner = dbConnection.createQueryRunner('master');
+
+    if (!accessToken) {
+      const result = await request(app.getHttpServer())
+        .post('/auth/sign-in')
+        .send({
+          email: 'test@gmail.com',
+          password: 'wecode1234',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/);
+      accessToken = result.body.data.accessToken;
+    }
 
     await queryRunner.startTransaction();
   });
