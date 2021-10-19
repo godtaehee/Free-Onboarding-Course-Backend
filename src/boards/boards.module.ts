e712f36a30865fb4498333fe50fa7409940866bb
taehee <wonderfulhuman@naver.com>
2021-10-19 16:06:50 +0900

    Feat: BoardsQueryRepository 사용을 위한 Module에 import

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,9 +4,13 @@ import { BoardsService } from './boards.service';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsRepository } from './boards.repository';
+import { BoardsQueryRepository } from './boards.query.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardsRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([BoardsRepository, BoardsQueryRepository]),
+    AuthModule,
+  ],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
