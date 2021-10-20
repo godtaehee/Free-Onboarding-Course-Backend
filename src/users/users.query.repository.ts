6bcbbf90d29e63c8ef5b27ef0b2de7fee47d7d4f
taehee <wonderfulhuman@naver.com>
2021-10-21 02:23:49 +0900

    Feat: User정보를 Pagination으로 가져오기 위한 로직 작성완료

--- src/users/users.query.repository.ts
+++ src/users/users.query.repository.ts
@@ -1,9 +1,30 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './users.entity';
+import { UserSearchRequest } from './dto/user.search.request';
 
 @EntityRepository(User)
 export class UsersQueryRepository extends Repository<User> {
   getSingleUserInfo(userId: number) {
     return this.createQueryBuilder('users').where({ id: userId }).getOne();
+  }
+
+  async getAllUserInfoUsingPagination(
+    userSearchRequest: UserSearchRequest,
+  ): Promise<[User[], number]> {
+    const coveringIndexQueryBuilder = this.createQueryBuilder('covers')
+      .select('covers.id')
+      .orderBy('covers.id', 'DESC')
+      .limit(userSearchRequest.getLimit())
+      .offset(userSearchRequest.getOffset());
+
+    return this.createQueryBuilder('users')
+      .innerJoin(
+        `(${coveringIndexQueryBuilder.getQuery()})`,
+        'covers',
+        'users.id = covers.id',
+      )
+      .leftJoinAndSelect('users.boards', 'boards')
+      .select(['boards', 'users'])
+      .getManyAndCount();
   }
 }
