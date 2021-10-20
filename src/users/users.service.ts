1d6bd84625ecc4474841c0447a9982c7db65ebdd
taehee <wonderfulhuman@naver.com>
2021-10-20 23:02:19 +0900

    Feat: UsersService에서 UsersQueryRepository사용을 위한 constructor에 DI

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersQueryRepository } from './users.query.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UsersQueryRepository)
+    private usersQueryRepository: UsersQueryRepository,
+  ) {}
+
   getSingleUserInfo(userId: number) {
-    return;
+    return this.usersQueryRepository.getSingleUserInfo(userId);
   }
 }
