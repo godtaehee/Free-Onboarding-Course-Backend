44bf6cb03e5db16921ab51bb2e7d75203fbc505d
taehee <wonderfulhuman@naver.com>
2021-10-20 23:08:32 +0900

    Feat: UsersService에 MockQueryRepository Provider로 주입

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersQueryRepository } from './users.query.repository';
 
+const mockQueryRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+};
 describe('UsersService', () => {
   let service: UsersService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UsersQueryRepository,
+          useValue: mockQueryRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
