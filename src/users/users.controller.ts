040ed41fb69297b67e1b29f9c2090a78165034da
taehee <wonderfulhuman@naver.com>
2021-10-21 02:23:19 +0900

    Feat: GET /users?offset&limit Route Handler에 Swagger Query Params 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Query, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserSearchRequest } from './dto/user.search.request';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
@@ -11,6 +12,17 @@ export class UsersController {
     return this.usersService.getSingleUserInfo(userId);
   }
 
+  @ApiQuery({
+    name: 'limit',
+    required: true,
+    description: '최대 몇개의 User 정보를 가져올것인지에 대한 값입니다.',
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: true,
+    description:
+      'User 정보의 페이지수를 나타냅니다. 0을 제외한 양수의 값입니다.',
+  })
   @Get('/')
   getAllUserInfoUsingPagination(
     @Query(new ValidationPipe({ transform: true }))
