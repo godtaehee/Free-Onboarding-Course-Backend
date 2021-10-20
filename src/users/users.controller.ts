b16876676128657ac766bdbbad0d0c0b108f25b3
taehee <wonderfulhuman@naver.com>
2021-10-21 00:41:31 +0900

    Feat: GET /users?offset&limit Route Handler추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserSearchRequest } from './dto/user.search.request';
 
 @Controller('users')
 export class UsersController {
@@ -8,5 +9,13 @@ export class UsersController {
   @Get('/:userId')
   async getSingleUserInfo(@Param('userId') userId: number) {
     return this.usersService.getSingleUserInfo(userId);
+  }
+
+  @Get('/')
+  getAllUserInfoUsingPagination(
+    @Query(new ValidationPipe({ transform: true }))
+    userSearchRequest: UserSearchRequest,
+  ) {
+    return this.usersService.getAllUserInfoUsingPagination(userSearchRequest);
   }
 }
