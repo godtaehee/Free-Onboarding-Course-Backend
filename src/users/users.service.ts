a1ba521a56759ba4030b0c92150b4d7687883e43
taehee <wonderfulhuman@naver.com>
2021-10-20 23:47:14 +0900

    Feat: 유효한 유저인지, 작성한 게시글이 있는지에 대한 여부를 판단하는 로직 작성

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersQueryRepository } from './users.query.repository';
+import { BoardsQueryRepository } from '../boards/boards.query.repository';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UsersQueryRepository)
     private usersQueryRepository: UsersQueryRepository,
+    @InjectRepository(BoardsQueryRepository)
+    private boardsQueryRepository: BoardsQueryRepository,
   ) {}
 
-  getSingleUserInfo(userId: number) {
-    return this.usersQueryRepository.getSingleUserInfo(userId);
+  async getSingleUserInfo(userId: number) {
+    const singleUserInfo = await this.usersQueryRepository.getSingleUserInfo(
+      userId,
+    );
+    if (!singleUserInfo)
+      throw new UnauthorizedException('요청을 처리할수 없습니다.');
+
+    const boardList = await this.boardsQueryRepository.getBoardListSpecificUser(
+      userId,
+    );
+
+    if (boardList) {
+      singleUserInfo.boards = boardList;
+      return singleUserInfo;
+    } else {
+      return singleUserInfo;
+    }
   }
 }
