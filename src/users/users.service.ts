9416a900227e1b74c3dc6588c64cb66721c8e94f
taehee <wonderfulhuman@naver.com>
2021-10-21 00:38:32 +0900

    Feat: Pagination Helper 주입 및 Pagination을 이용한 User정보를 가져올때 최종 UserResponse를 만들어주는 로직 작성

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,20 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersQueryRepository } from './users.query.repository';
 import { BoardsQueryRepository } from '../boards/boards.query.repository';
+import { UserSearchRequest } from './dto/user.search.request';
+import { User } from './users.entity';
+import { PaginationHelper } from '../common/utils/pagination.helper';
+import { UserResponse } from '../common/response/user/user.response';
+
+interface AllUserInfoUsingPagination {
+  userSearchRequest: UserSearchRequest;
+}
 
 @Injectable()
 export class UsersService {
@@ -10,6 +23,8 @@ export class UsersService {
     private usersQueryRepository: UsersQueryRepository,
     @InjectRepository(BoardsQueryRepository)
     private boardsQueryRepository: BoardsQueryRepository,
+    @Inject(PaginationHelper)
+    private paginationHelper: PaginationHelper<User>,
   ) {}
 
   async getSingleUserInfo(userId: number) {
@@ -29,5 +44,20 @@ export class UsersService {
     } else {
       return singleUserInfo;
     }
+  }
+
+  async getAllUserInfoUsingPagination(userSearchRequest: UserSearchRequest) {
+    const [users, count] =
+      await this.usersQueryRepository.getAllUserInfoUsingPagination(
+        userSearchRequest,
+      );
+
+    if (users.length <= 0)
+      throw new BadRequestException(`유저가 존재하지 않습니다.`);
+    return this.paginationHelper.getPaginationItems<UserResponse>(
+      count,
+      userSearchRequest.limit,
+      users.map((u) => new UserResponse(u, u.boards)),
+    );
   }
 }
