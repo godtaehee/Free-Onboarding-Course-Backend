bdb6e0410865e7c4df7b0672106f053fefd8e2d5
taehee <wonderfulhuman@naver.com>
2021-10-21 12:51:03 +0900

    Feat: 회원가입에서 사용하는 응답이 아닌 유저에관한 요청에 범용적으로 사용하기위한 이름변경

--- src/common/response/user/request.about.user.success.response.ts
+++ src/common/response/user/request.about.user.success.response.ts
@@ -1,9 +1,9 @@
 import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 
 @ApiExtraModels()
-export class RegisterSuccessResponse {
+export class RequestAboutUserSuccessResponse {
   @ApiProperty({
-    description: '회원가입이 성공한후 Auto-Generated된 유저의 고유 id값입니다.',
+    description: 'User의 Auto-Generated된 고유 id입니다.',
     example: '1',
   })
   userId: number;
