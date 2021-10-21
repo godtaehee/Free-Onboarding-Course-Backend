12244702ce495754dcb6d5703e127c094d4541dd
taehee <wonderfulhuman@naver.com>
2021-10-21 14:34:57 +0900

    Feat: Route handler의 위치배정은 static한 Route Handler가 최상위에 와야한다.

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -55,6 +55,17 @@ export class BoardsController {
 
   tag: string;
 
+  @ApiQuery({
+    name: 'id',
+    required: true,
+    description:
+      '특정 유저가 작성한 게시글을 가져오기위한 유저의 아이디입니다.',
+  })
+  @Get('/user')
+  getBoardListSpecificUser(@Query('id') userId: number) {
+    return this.boardsService.getBoardListSpecificUser(userId);
+  }
+
   @ApiBearerAuth('access-token')
   @ApiOperation({
     summary: '게시글을 생성합니다.',
@@ -82,68 +93,6 @@ export class BoardsController {
       }' request board-create`,
     );
     return this.boardsService.createBoard(user, boardCreateDto);
-  }
-
-  @ApiOperation({
-    summary: '게시글의 id를 기준으로 읽습니다.',
-    description: '한개의 게시물을 읽어옵니다.',
-  })
-  @ApiParam({
-    name: 'boardId',
-    required: true,
-    description: '읽어올 게시글의 고유 번호입니다.',
-  })
-  @ApiCommonOkResponseForm(NotIncludeSensitiveBoardInfoResponse, {
-    description: '성공적으로 Board를 가져왔을때의 응답입니다.',
-  })
-  @ApiBadRequestResponse({
-    type: NotValidNumberError,
-  })
-  @UseInterceptors(CommonResponseFormInterceptor)
-  @Get('/:boardId')
-  getSingleBoard(
-    @Param('boardId', PositiveNumberValidationPipe) boardId: number,
-  ): Promise<Board> {
-    this.logger.debug(
-      `${this.tag} ${new Date()} '${boardId}'번째 게시물을 읽어옵니다.`,
-    );
-    return this.boardsService.getSingleBoard(boardId);
-  }
-
-  @ApiOperation({
-    summary: '게시글을 최대 limit개 단위로 가져옵니다. (Pagination)',
-    description:
-      '게시글을 최대 limit개 단위로 가져옵니다. Pagination을 구현하실때 사용하실수 있습니다. ' +
-      '삭제된 게시글은 가져올수 없습니다. 최신 -> 과거순으로 Board를 가져옵니다.',
-  })
-  @ApiQuery({
-    name: 'limit',
-    required: true,
-    description: '최대 몇개의 Board를 가져올것인지에 대한 값입니다.',
-  })
-  @ApiQuery({
-    name: 'offset',
-    required: true,
-    description:
-      '게시판의 시작페이지의 수를 나타냅니다. 0을 제외한 양수의 값입니다.',
-  })
-  @ApiCommonPaginationOkResponseForm(NotIncludeSensitiveBoardInfoResponse, {
-    description:
-      '유저의 패스워드와 같은 민감한정보를 포함하지않는 Board의 응답입니다.',
-  })
-  @ApiBadRequestResponse({
-    type: NotValidNumberError,
-  })
-  @Get('/')
-  getAllBoard(
-    @Query(new ValidationPipe({ transform: true })) query: BoardSearchRequest,
-  ): Promise<Page<NotIncludeSensitiveBoardInfoResponse>> {
-    this.logger.debug(
-      `${this.tag} ${new Date()} ${
-        (query.offset - 1) * query.limit
-      }번째부터 최대 '${query.limit}'개의 게시물을 읽어옵니다.`,
-    );
-    return this.boardsService.getAllBoard(query);
   }
 
   @ApiBearerAuth('access-token')
@@ -222,8 +171,65 @@ export class BoardsController {
     return this.boardsService.deleteBoard(user.userId, boardId);
   }
 
-  @Get('/user')
-  getBoardListSpecificUser(@Query('id') userId: number) {
-    return this.boardsService.getBoardListSpecificUser(userId);
+  @ApiOperation({
+    summary: '게시글의 id를 기준으로 읽습니다.',
+    description: '한개의 게시물을 읽어옵니다.',
+  })
+  @ApiParam({
+    name: 'boardId',
+    required: true,
+    description: '읽어올 게시글의 고유 번호입니다.',
+  })
+  @ApiCommonOkResponseForm(NotIncludeSensitiveBoardInfoResponse, {
+    description: '성공적으로 Board를 가져왔을때의 응답입니다.',
+  })
+  @ApiBadRequestResponse({
+    type: NotValidNumberError,
+  })
+  @UseInterceptors(CommonResponseFormInterceptor)
+  @Get('/:boardId')
+  getSingleBoard(
+    @Param('boardId', PositiveNumberValidationPipe) boardId: number,
+  ): Promise<Board> {
+    this.logger.debug(
+      `${this.tag} ${new Date()} '${boardId}'번째 게시물을 읽어옵니다.`,
+    );
+    return this.boardsService.getSingleBoard(boardId);
+  }
+
+  @ApiOperation({
+    summary: '게시글을 최대 limit개 단위로 가져옵니다. (Pagination)',
+    description:
+      '게시글을 최대 limit개 단위로 가져옵니다. Pagination을 구현하실때 사용하실수 있습니다. ' +
+      '삭제된 게시글은 가져올수 없습니다. 최신 -> 과거순으로 Board를 가져옵니다.',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: true,
+    description: '최대 몇개의 Board를 가져올것인지에 대한 값입니다.',
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: true,
+    description:
+      '게시판의 시작페이지의 수를 나타냅니다. 0을 제외한 양수의 값입니다.',
+  })
+  @ApiCommonPaginationOkResponseForm(NotIncludeSensitiveBoardInfoResponse, {
+    description:
+      '유저의 패스워드와 같은 민감한정보를 포함하지않는 Board의 응답입니다.',
+  })
+  @ApiBadRequestResponse({
+    type: NotValidNumberError,
+  })
+  @Get('/')
+  getAllBoard(
+    @Query(new ValidationPipe({ transform: true })) query: BoardSearchRequest,
+  ): Promise<Page<NotIncludeSensitiveBoardInfoResponse>> {
+    this.logger.debug(
+      `${this.tag} ${new Date()} ${
+        (query.offset - 1) * query.limit
+      }번째부터 최대 '${query.limit}'개의 게시물을 읽어옵니다.`,
+    );
+    return this.boardsService.getAllBoard(query);
   }
 }
