cfd21414d4fe16def40549bd3370e87f6a540bfd
taehee <wonderfulhuman@naver.com>
2021-10-21 16:55:45 +0900

    Feat: Board Type이 아닌 User정보를 포함하지않는  PickType<Board> 타입의 NotIncludeUserInfoResponse를 사용해야하며, boards의 반환값은 배열이 되야한다.

--- src/common/response/user/user.response.ts
+++ src/common/response/user/user.response.ts
@@ -1,4 +1,9 @@
-import { ApiExtraModels, ApiProperty, PickType } from '@nestjs/swagger';
+import {
+  ApiExtraModels,
+  ApiProperty,
+  getSchemaPath,
+  PickType,
+} from '@nestjs/swagger';
 import { Board } from '../../../boards/boards.entity';
 import { User } from '../../../users/users.entity';
 import { NotIncludeUserInfoResponse } from '../board/not.include.user.info.response';
@@ -26,10 +31,13 @@ export class UserResponse extends PickType(User, [
   }
 
   @ApiProperty({
-    type: () => NotIncludeUserInfoResponse,
+    type: 'array',
+    items: {
+      $ref: getSchemaPath(NotIncludeUserInfoResponse),
+    },
     description:
       '해당 유저가 작성한 모든 게시글을 나타냅니다. ' +
       'User의 정보에 들어가는 Board의 정보이므로 기존의 Board에 있던 User의 정보는 들어가지않습니다',
   })
-  boards: Board[];
+  boards: NotIncludeUserInfoResponse[];
 }
