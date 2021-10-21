8457b9e6e2696739ac147cc18e20a09ec6a54a7f
taehee <wonderfulhuman@naver.com>
2021-10-21 18:33:34 +0900

    Feat: User Route Handler에 Swagger설명 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -44,6 +44,7 @@ export class UsersController {
   })
   @ApiBadRequestResponse({
     type: NotValidNumberError,
+    description: '제약조건에 맞지 않은 값이 포함되어 있습니다.',
   })
   @UseInterceptors(CommonResponseFormInterceptor)
   @Get('/:userId')
@@ -54,7 +55,7 @@ export class UsersController {
   @ApiTags('In-Memory Database 확인용')
   @ApiOperation({
     summary:
-      '유저의 정보를 Pagination을 이용해 서비스의 모든 유저정보가 아닌 일정한 단위의 유저정보를 가져옵니다.',
+      '유저의 정보를 Pagination을 이용해 서비스의 모든 유저정보가 아닌 일정한 단위의 유저정보를 가져옵니다. (Pagination)',
     description:
       '채점자분들께서 DB의 실제 내용을 확인하기 위해 DB Browser for SQLite와 같은' +
       ' In-Memory DB를 가시적으로 볼 수 있는 프로그램도 설치하지 않아도 확인하실 수 있는 것에 목적을 둔 API입니다.' +
@@ -63,19 +64,24 @@ export class UsersController {
   @ApiQuery({
     name: 'limit',
     required: true,
-    description: '최대 몇 개의 User 정보를 가져올 것인지에 대한 값입니다.',
+    description:
+      '최대 몇 개의 User 정보를 가져올 것인지에 대한 값입니다.' +
+      '최대 50개의 유저정보를 가져올 수 있습니다.',
   })
   @ApiQuery({
     name: 'offset',
     required: true,
     description:
-      'User 정보의 페이지 수를 나타냅니다. 0을 제외한 양수의 값입니다.',
+      'User 정보의 페이지 수를 나타냅니다. 0을 제외한 양수의 값입니다. ',
   })
   @ApiCommonPaginationOkResponseForm(UserResponse, {
-    description: 'limit 개의 유저에 대한 정보입니다.',
+    description:
+      'limit 개의 유저에 대한 정보입니다. ' +
+      `아직 글을 작성하지 않은 작성자라면 boards 값은 빈 배열이 됩니다.`,
   })
   @ApiBadRequestResponse({
     type: NotValidNumberError,
+    description: '해당 페이지에 해당하는 User 정보가 없습니다.',
   })
   @Get('/')
   getAllUserInfoUsingPagination(
