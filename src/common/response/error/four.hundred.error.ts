6038e0f2cb79e369f1048907248992db855a6d37
taehee <wonderfulhuman@naver.com>
2021-10-21 18:34:07 +0900

    Feat: class-validator를 통한 다양한 제약조건의 오류메시지가 있을수 있으므로 몇개의 오류메시지를 더 추가

--- src/common/response/error/four.hundred.error.ts
+++ src/common/response/error/four.hundred.error.ts
@@ -12,12 +12,15 @@ export class FourHundredError {
   @ApiProperty({
     type: 'array',
     title: 'Error 메시지',
-    items: {
-      type: 'string',
-      example: 'title must be longer than or equal to 2 charaters',
-    },
+    example: [
+      'title must be longer than or equal to 2 charaters',
+      'email must be an email',
+      'password must be longer than or equal to 5 characters',
+      'nickname must be shorter than or equal to 20 characters',
+    ],
     description:
-      'Request-Body의 각각 프로퍼티의 제약조건에 맞지 않은 값이 포함되어 있습니다.',
+      'Request-Body의 각각 프로퍼티의 제약조건에 맞지 않은 값이 포함되어 있습니다. ' +
+      '해당 예시에 나와 있는 오류중에 적어도 한 개의 조건에 맞지 않아 요청이 실패했습니다.',
   })
   message: any;
 
--- src/common/response/error/not.valid.number.error.ts
+++ src/common/response/error/not.valid.number.error.ts
@@ -10,8 +10,7 @@ export class NotValidNumberError extends PickType(FourHundredError, [
     type: 'string',
     title: 'Error 메시지',
     example: '잘못된 요청입니다.',
-    description:
-      'Request-Body의 각각 프로퍼티의 제약조건에 맞지 않은 값이 포함되어 있습니다.',
+    description: '제약조건에 맞지 않은 값이 포함되어 있습니다.',
   })
   message: any;
 }
